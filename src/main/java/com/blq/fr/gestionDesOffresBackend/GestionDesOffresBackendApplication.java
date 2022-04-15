6e4c5cb29244f02ad305b9b84dc3fc40d45928b4
Clement <Clement@192.168.100.40>
2022-04-15 11:15:22 +0200

    Commit d'initialisation pour lier le projet au github.

---
+++ src/main/java/com/blq/fr/gestionDesOffresBackend/GestionDesOffresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blq.fr.gestionDesOffresBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesOffresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesOffresBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blq/fr/gestionDesOffresBackend/GestionDesOffresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blq.fr.gestionDesOffresBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesOffresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
